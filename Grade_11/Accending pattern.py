3bd723716ecd1cfb9d8707f6c76d6c8c64a2a3f4
Designerpro13 <111132850+Designerpro13@users.noreply.github.com>
2023-07-11 02:03:24 +0530

    relocating file

--- Grade_11/Accending pattern.py
+++ Grade_11/Accending pattern.py
@@ -1,8 +1,7 @@
 for i in range(5):
     for j in range(5):
         if i==j or i>j:
-         print('8',end='')
-         i-=2
+         print('*',end='')
+         i-=1
         else:
-             )
-        print('')
+            print('')
