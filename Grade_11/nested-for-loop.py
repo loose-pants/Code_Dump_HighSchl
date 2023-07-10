4a43c4a55c93e3578be40adc53f02b857fbaa2b2
Designerpro13 <111132850+Designerpro13@users.noreply.github.com>
2023-07-11 02:20:07 +0530

    relocating file

---
+++ Grade_11/nested-for-loop.py
@@ -0,0 +1,7 @@
+
+Number_of_loops_occured=0
+for i in range(5):
+	for j in range(4):
+		print(i,'-',j)
+		Number_of_loops_occured+=1
+print(Number_of_loops_occured)
--- nested loop intor.py
+++
@@ -1,6 +0,0 @@
-count=0
-for i in range(5):
-	for j in range(4):
-		print(i,'-',j)
-		count=count+1
-print(count)
