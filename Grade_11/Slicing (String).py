a355cb758c31c5a6b927416d5293bba1cc56663b
Designerpro13 <111132850+Designerpro13@users.noreply.github.com>
2023-07-11 02:12:45 +0530

    relocating file

---
+++ Grade_11/Slicing (String).py
@@ -0,0 +1,5 @@
+#Slicing
+name='Hello'
+print(name[0:5:2])
+'''<identifier>[beggning-index:end-index:step]
+Note end-index return n-1th index value'''
--- Slicing (String).py
+++
@@ -1,3 +0,0 @@
-#Slicing
-name='Hello'
-print(name[0:5:2])
